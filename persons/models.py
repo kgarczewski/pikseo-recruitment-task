32e07d2e09dfea3c4051affd5359de39c77277dc
k.garczewski <garczewskikrzysztof@gmail.com>
2024-05-28 20:07:47 +0200

    task-3

--- persons/models.py
+++ persons/models.py
@@ -4,9 +4,15 @@ from django.db import models
 class Skills(models.Model):
     name = models.CharField(verbose_name="Imię", max_length=64, unique=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Position(models.Model):
     name = models.CharField(verbose_name="Stanowisko", max_length=64, unique=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Persons(models.Model):
@@ -17,3 +23,6 @@ class Persons(models.Model):
         Position, verbose_name="Stanowisko", null=True, on_delete=models.SET_NULL
     )
     age = models.IntegerField(verbose_name="Wiek", null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
